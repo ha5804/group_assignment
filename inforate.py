27651323e87a7a409d4eab68d832173a386904e3
ha5804 <zixpotf5803@gmail.com>
2025-05-31 18:56:16 +0900

    환율값만 불러오는법 반복 확인중

--- inforate.py
+++ inforate.py
@@ -14,7 +14,8 @@ user_input = user_input.split(',')
 
 for i in user_input:
     row = data[data[f"국가별"] == i]
-    print(row.dtypes())
+    
+    
 
 # while True:
 #     user_input2 = input("데이터 시각화를 희망하시면 yes, 아니면 no라고 입력하세요: ")
