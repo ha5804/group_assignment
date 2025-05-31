accfba702780a0fd2c34a0a88fa00b74c5028b59
0954afgb <0954afgb@gmail.com>
2025-05-31 21:14:13 +0900

    content를 더 입력할 수 있는지 디버깅해보기기

--- llm.py
+++ llm.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 
 user_order = input("환율에 대한 무엇이 궁금하시나요?: ")
-
+me = 2
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key="<OPENROUTER_API_KEY>",
@@ -13,7 +13,8 @@ completion = client.chat.completions.create(
   messages=[
     {
       "role": "user",
-      "content": ,
+      "content": user_order,
+      "content": me
       "role": "system",
       "content": "너는 환율에 대한 답변을 해주는 어시스턴트야. "
     }
