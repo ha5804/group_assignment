108ccd28f1684e62aafa25e1ab8d02660ed8251f
0954afgb <0954afgb@gmail.com>
2025-05-31 21:36:40 +0900

    content 추가 작업 중중

--- llm.py
+++ llm.py
@@ -1,10 +1,10 @@
 from openai import OpenAI
 
 
-user_order = input("이 국가들의 환율에 대해해 무엇이 궁금하시나요?: ")
+user_order = input("이 국가들의 환율에 대해 무엇이 궁금하시나요?: ")
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="<OPENROUTER_API_KEY>",
+  api_key="",
 )
 
 completion = client.chat.completions.create(
@@ -18,9 +18,9 @@ completion = client.chat.completions.create(
     {
       "role": "user",
       "content": user_order,
-      "content": rate_dic,  
-      "role": "system",
-      "content": "너는 환율에 대한 답변을 해주는 어시스턴트야."
+    },  
+      {"role": "system",
+      "content": "너는 환율에 대한 답변을 해주는 어시스턴트야. rate_dic 값을 바탕으로 답변을 해줘."
     }  
   ]
 )
