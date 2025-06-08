6d8cee3362be76ad846d39974a67888e8e078f55
ha5804 <zixpotf5803@gmail.com>
2025-06-08 18:20:03 +0900

    .

--- inforate.py
+++ inforate.py
@@ -35,29 +35,49 @@ rate_list = [] #y축에 들어갈 환율값
 for i in user_input:
     rate = data[data["국가별"] == i]
     rate_list.append(rate)
+# max_list = []
+# for i in range(len(rate_list)):
+#     max = 0
+#     for j in range(10):
+#         if (rate_list[i].iloc[0][j+1] >= max):
+#             max = rate_list[i].iloc[0][j]
+#     max_list.append(max)
+
+# min_list = []
+for i in range(len(rate_list)):
+    a = rate_list[i].iloc[0][1:]
+    print(a)
     
-while True:
-    print()
-    user_input2 = input("데이터 시각화를 희망하시면 yes, 아니면 no라고 입력하세요: ")
-    if user_input2 == "no":
-       break
-    elif user_input2 == "yes":
-        plt.figure(figsize = (10, 6))
-        plt.rc("font", family = "AppleGothic")
-        plt.plot(year, rate_list[0].iloc[0][1:], label = user_country[0], color = 'red', marker = '.')
-        plt.plot(year, rate_list[1].iloc[0][1:], label = user_country[1], color = 'orange', marker = '.')
-        plt.plot(year, rate_list[2].iloc[0][1:], label = user_country[2], color = 'green', marker = '.')
-        plt.plot(year, rate_list[3].iloc[0][1:], label = user_country[3], color = 'blue', marker = '.')
-        plt.plot(year, rate_list[4].iloc[0][1:], label = user_country[4], color = 'purple', marker = '.')
-        plt.xlabel("year")
-        plt.ylabel("rate")
-        plt.legend(loc = "upper right")
-        plt.xticks(year, rotation = 45)
-        plt.grid(True, linestyle = ":")
-        plt.show()
-        break
-    else:
-        print("입력 형식을 다시 확인해주세요!")
+    
+
+
+
+    
+    
+    
+
+# while True:
+#     print()
+#     user_input2 = input("데이터 시각화를 희망하시면 yes, 아니면 no라고 입력하세요: ")
+#     if user_input2 == "no":
+#        break
+#     elif user_input2 == "yes":
+#         plt.figure(figsize = (10, 6))
+#         plt.rc("font", family = "AppleGothic")
+#         plt.plot(year, rate_list_norm[], label = user_country[0], color = 'red', marker = '.')
+#         plt.plot(year, rate_list[1].iloc[0][1:], label = user_country[1], color = 'orange', marker = '.')
+#         plt.plot(year, rate_list[2].iloc[0][1:], label = user_country[2], color = 'green', marker = '.')
+#         plt.plot(year, rate_list[3].iloc[0][1:], label = user_country[3], color = 'blue', marker = '.')
+#         plt.plot(year, rate_list[4].iloc[0][1:], label = user_country[4], color = 'purple', marker = '.')
+#         plt.xlabel("year")
+#         plt.ylabel("rate")
+#         plt.legend(loc = "upper right")
+#         plt.xticks(year, rotation = 45)
+#         plt.grid(True, linestyle = ":")
+#         plt.show()
+#         break
+#     else:
+#         print("입력 형식을 다시 확인해주세요!")
 
 
 
