d2134a814101119b4429bd73a15e45b49dbfcc51
ha5804 <zixpotf5803@gmail.com>
2025-06-08 16:23:04 +0900

    코드오류 수정 및 새로운방식 적용해보기

--- matplot.debug.py
+++ matplot.debug.py
@@ -2,5 +2,9 @@ import matplotlib as plt
 import pandas as pd
 my_data = pd.read_csv("rate.csv")
 x = my_data["국가별"]
+user_input = input().replace(' ','')
+user_input = user_input.split(',')
+
 for i in x:
-    print(i)
+    if (i in user_input):
+        print(i)
