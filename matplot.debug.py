ba9ee4b1f711a53e403d21eb084c634ff452a36a
ha5804 <zixpotf5803@gmail.com>
2025-06-08 16:47:47 +0900

    딕셔너리반환 하지않고 판다스 자체로 플랏 코드 작성

--- matplot.debug.py
+++ matplot.debug.py
@@ -1,10 +1,22 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 my_data = pd.read_csv("rate.csv")
 x = my_data["국가별"]
 user_input = input().replace(' ','')
 user_input = user_input.split(',')
 
+contry = []
 for i in x:
     if (i in user_input):
-        print(i)
+        contry.append(i)
+x_list = []
+for i in user_input:
+    year = my_data[my_data["국가별"] == i]
+    x_list.append(year)
+print(x_list)
+print(contry)
+year = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+plt.figure(figsize = (10, 6))
+plt.plot(year, x_list[0].iloc[0][1:])
+plt.plot(year, x_list[1].iloc[0][1:])
+plt.show()
