50a6092af0e8ef4ca582d56ccd8d04a77dcdd149
ha5804 <zixpotf5803@gmail.com>
2025-06-08 17:12:17 +0900

    예외처리 문장 추가하고 형식 정리하기

--- inforate.py
+++ inforate.py
@@ -6,11 +6,17 @@ print("과거의 환율 변동을 통한 유익한 정보를 제공하는 프로
 "조회하고 싶은 국가들중 미국을 포함한 5개의 국가를 입력하세요. :) \n" \
 "입력형식은 희망하는 국가들을 쉼표(,)를 기준으로 작성해주세요! \n" 
 "예시) 미국, 대한민국, 중국, 일본, 베트남")
+print()
 
-
-user_input = input("입력: ").replace(' ','')
-user_input = user_input.split(',')
-#print(user_input)
+while True:
+    user_input = input("입력: ").replace(' ','')
+    user_input = user_input.split(',')
+    if len(user_input) == 5:
+        break
+    else:
+        print("국가 입력갯수는 5개입니다.")
+        print()
+    
 
 year = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
 x = data["국가별"]
@@ -26,6 +32,7 @@ for i in user_input:
     rate_list.append(rate)
     
 while True:
+    print()
     user_input2 = input("데이터 시각화를 희망하시면 yes, 아니면 no라고 입력하세요: ")
     if user_input2 == "no":
        False
