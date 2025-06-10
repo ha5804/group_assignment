316e100cca18afa00280177be6b090fa8f6636bd
0954afgb <0954afgb@gmail.com>
2025-06-10 11:16:31 +0900

    국가 환율 전망 코드 생성

---
+++ exchange_rate_forecast.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+
+input_country = input("국가: ")
+if input_country in x:
+    wanting_country = input_country
+    
+
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="",
+)
+
+completion = client.chat.completions.create(
+  extra_headers={
+    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+  },
+  extra_body={},  
+  model="meta-llama/llama-3.3-8b-instruct:free",
+  messages=[
+    {
+      "role": "user",
+      "content": 
+    },  
+    {
+      "role": "system",
+      "content": ""
+    }  
+  ]
+)
+print(completion.choices[0].message.content)
