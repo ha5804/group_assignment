29a97c267ad0b5989a3de6fe5ae2a226bc5df77c
0954afgb <0954afgb@gmail.com>
2025-06-11 10:02:24 +0900

    답변에 대한 평가 프로그램

--- exchange_rate_forecast.py
+++ exchange_rate_forecast.py
@@ -23,5 +23,14 @@ while True:
        user_count += 1
        print(completion.choices[0].message.content)
 
+score_1 = []
+score_2 = []
+score_3 = []
 
+x_label = ["spped", "accuracy", "clarity", "mean_score"]
+print(f"{count_question}번의 질문에 대한 간단한 설문 부탁드립니다!\n""다음 질문에 대해서 1~10점 사이로 숫자만 작성해주세요.")
 
+score_1.append(int(input("답변의 속도는 적당했나요?: ")))
+score_2.append(int(input("질문에 대한 궁금증이 해결되었나요?: ")))
+score_3.append(int(input("답변이 너무 어렵지 않았나요?: ")))
+print("설문에 응해주셔서 감사합니다!")
