ab7385b5955d729796d9d968a604578563cdf526
0954afgb <0954afgb@gmail.com>
2025-06-11 09:56:35 +0900

    while문 완성성

--- exchange_rate_forecast.py
+++ exchange_rate_forecast.py
@@ -14,25 +14,14 @@ while True:
     user_q = input("환율에 관한 궁금한 점들을 물어보세요!\n""종료를 희망하시면 exit이라고 적어주세요: ")
     if(user_q.lower() == "exit"):
         break
-  
-
-
-completion = client.chat.completions.create(
-  extra_headers={
-    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
-    "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
-  },
-  extra_body={},  
-  model="meta-llama/llama-3.3-8b-instruct:free",
-  messages=[
-    {
-      "role": "system",
-      "content": 
-    }
-  ]
-)
-print(completion.choices[0].message.content)
+    
+    else:
+       completion = client.chat.completions.create(
+          model="meta-llama/llama-3.3-8b-instruct:free",
+          messages=[{"role": "system","content": user_q}])
+       count_question += 1
+       user_count += 1
+       print(completion.choices[0].message.content)
 
 
 
-completion.choices[0].message.content
