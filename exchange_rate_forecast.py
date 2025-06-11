88bdaa9027fe650147a852a626d329144bc183f1
0954afgb <0954afgb@gmail.com>
2025-06-11 09:48:17 +0900

    환율에 대한 답변 및 평가 프로그램 생성 기초

--- exchange_rate_forecast.py
+++ exchange_rate_forecast.py
@@ -1,23 +1,15 @@
 from openai import OpenAI
 import matplotlib.pyplot as plt
 
-while True:
-   input_country = input("국가: ")
-   if input_country in x:
-     break
-   else: 
-      print("유효한 국가를 입력해주세요.")
-
-c_rate = data[data["국가별"] == input_country]
-b = c_rate.iloc[0][1:]
-  
-    
-
-
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key="",
 )
+
+
+count_question = 0
+user_count = 10
+
 
 completion = client.chat.completions.create(
   extra_headers={
@@ -28,13 +20,9 @@ completion = client.chat.completions.create(
   model="meta-llama/llama-3.3-8b-instruct:free",
   messages=[
     {
-      "role": "user",
-      "content": b
-    },  
-    {
       "role": "system",
-      "content": "다음 데이터를 기반으로 향후 6개월 간의 환율 전망을 제공해주세요. 전망은 월별로 명확하게 제시하고, JSON 형식으로 제공해 주세요."
-    }  
+      "content": 
+    }
   ]
 )
 print(completion.choices[0].message.content)
